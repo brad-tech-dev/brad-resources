424509b746c91485ec0b046135e13cb8957da063
BRAD Technologies <brad.technologies@outlook.com>
2019-05-19 13:03:14 +0300

    Mapped resources to enum variable.

--- brad-image-resources/src/main/java/brad/tech/res/img/ImageResource.java
+++ brad-image-resources/src/main/java/brad/tech/res/img/ImageResource.java
@@ -4,54 +4,316 @@ import java.net.URL;
 
 public enum ImageResource {
 
-    // a
-    ADD_COLUMN("add-column"),
-    ADD_DATABASE("add-database"),
-    ADD_NODE("add-node"),
-    ADD_SUBNODE("add-subnode"),
-    ADD_TAB("add-tab"),
-    ADJUST("adjust"),
-    ADMINISTRATOR_MALE("administrator-male")
+    // A
+    ADD_COLUMN("add-column.png"),
+    ADD_DATABASE("add-database.png"),
+    ADD_NODE("add-node.png"),
+    ADD_SUBNODE("add-subnode.png"),
+    ADD_TAB("add-tab.png"),
+    ADJUST("adjust.png"),
+    ADMINISTRATOR_MALE("administrator-male.png"),
+    ALIGN_TEXT_BOTTOM("align-text-bottom.png"),
+    ALIGN_TEXT_CENTER("align-text-center.png"),
+    ALIGN_TEXT_LEFT("align-text-left.png"),
+    ALIGN_TEXT_RIGHT("align-text-right.png"),
+    ALIGN_TEXT_TOP("align-text-top.png"),
+    APPLICATION_WINDOW("application-window.png"),
+    APPROVE("approve.png"),
 
+    // B
+    BUS("bus.png"),
+    BAR_CHART("bar-chart.png"),
+    BASKET("basket.png"),
+    BINDER("binder.png"),
 
-    // b
-    // c
-    // d
-    // e
-    // f
-    // g
-    // h
-    // i
-    // j
-    // k
-    // l
-    // m
-    // n
-    // o
-    // p
-    // q
-    // r
-    // s
-    // t
-    // u
-    // v
-    // w
-    // x
-    // y
-    // z
-    ;
+    // C
+    CALCULATOR("calculator.png"),
+    CALENDER_DELETE("calender-delete.png"),
+    CALENDER_MINUS("calender-minus.png"),
+    CALENDER_PLUS("calender-plus.png"),
+    CANCEL("cancel.png"),
+    CASH_REGISTER("cash-register.png"),
+    CELLS("cells.png"),
+    CHANGE_THEME("change-theme.png"),
+    CHROME("chrome.png"),
+    CLEAR_FILTERS("clear-filters.png"),
+    CLEAR_SEARCH("clear-search.png"),
+    CLEAR_SYMBOL("clear-symbol.png"),
+    CLIENT_MANAGEMENT("client-management.png"),
+    CLOCK("clock.png"),
+    CLOSE_ALL_TABS("close-all-tabs.png"),
+    CLOSE_PANE("close-pane.png"),
+    CLOSE_TAB("close-tab.png"),
+    CLOSE_WINDOW("close-window.png"),
+    COLUMNS("columns.png"),
+    COMBO_CHART("combo-chart.png"),
+    COMPLAINTS("complaints.png"),
+    COMPUTER("computer.png"),
+    COMPUTER_MONITOR("computer-monitor.png"),
+    COMPITER_SUPPORT("computer-support.png"),
+    CONFIGURATION("configuration.png"),
+    CONNECTED("connected.png"),
+    CONVERSION("conversion.png"),
 
-    private String resource;
+    // D
+    DATA("data.png"),
+    DATA_BACKUP("data-backup.png"),
+    DATA_CONFIGURATION("data-configuration.png"),
+    DATA_ENCRYPTION("data-encryption.png"),
+    DATA_PROTECTION("data-protection.png"),
+    DATA_QUALITY("data-quality.png"),
+    DATA_RECOVERY("data-recovery.png"),
+    DATABASE("database.png"),
+    DATABASE_ADMINISTRATOR("database-administrator.png"),
+    DATABASE_EXPORT("database-export.png"),
+    DATABASE_RESTORE("database-restore.png"),
+    DATABASE_VIEW("database-view.png"),
+    DATE_FROM("date-from.png"),
+    DATE_TO("date-to.png"),
+    DELETE_BIN("delete-bin.png"),
+    DELETE_COLUMN("delete-column.png"),
+    DELETE_DATABASE("delete-database.png"),
+    DELETE_NODE("delete-node.png"),
+    DELETE_ROW("delete-row.png"),
+    DELETE_SUBNODE("delete-subnode.png"),
+    DELETE_TABLE("delete-table.png"),
+    DELETE_TICKET("delete-ticket.png"),
+    DESKTOP("desktop.png"),
+    DISAPPROVE("disapprove.png"),
+    DOWNLOAD("download.png"),
+    DOWNLOADING_UPDATES("downloading-updates.png"),
+    DOWNLOADS("downloads.png"),
 
-    ImageResource(String resource) {
-        this.resource = resource;
+    // E
+    EDIT_COLUMN("edit-column.png"),
+    EDIT_NODE("edit-node.png"),
+    EDIT_ROW("edit-row.png"),
+    EMAIL("email.png"),
+    EMPLOYEE("employee.png"),
+    ENTER("enter.png"),
+    ERROR("error.png"),
+    ETHERNET_ON("ethernet-on.png"),
+    EVENT_ACCEPTED_TENTATIVELY("event-accepted-tentatively.png"),
+    EXCHANGE("exchange.png"),
+    EXIT("exit.png"),
+    EXPIRED("expired.png"),
+    EXPORT("export.png"),
+    EXPORT_CSV("export-csv.png"),
+
+    // F
+    FACE("face-id.png"),
+    FACIAL_RECOGNITION("facial-recognition.png"),
+    FAQ("faq.png"),
+    FEMALE_PROFILE("female-profile.png"),
+    FILING_CABINET("filing-cabinet.png"),
+    FILTER("filter.png"),
+    FIND_AND_REPLACE("find-and-replace.png"),
+    FINGERPRINT("fingerprint.png"),
+    FINGERPRINT_SCAN("fingerprint-scan.png"),
+    FIREFOX("firefox.png"),
+    FORM("form.png"),
+    FRIDAY("friday.png"),
+    FUTURE("future.png"),
+
+    // G
+    GAS("gas.png"),
+    GAS_PUMP("gas-pump.png"),
+    GAS_STATION("gas-station.png"),
+    GOOGLE_SHEETS("google-sheets.png"),
+    GRID_2("grid-2.png"),
+    GRID_3("grid-3.png"),
+
+    // H
+    HELP("help.png"),
+    HIDE("hide.png"),
+    HIGH_PRIORITY("high-priority.png"),
+    HINT("hint.png"),
+
+    // I
+    IMPORT("import.png"),
+    IMPORT_CSV("import-csv.png"),
+    INFO_POPUP("info-popup.png"),
+    INFORMATION("information.png"),
+    INPUT("input.png"),
+    INSERT_COLUMN_LEFT("insert-column-left.png"),
+    INSERT_COLUMN_RIGHT("insert-column-right.png"),
+    INSERT_ROW_ABOVE("insert-row-above.png"),
+    INSERT_TABLE("insert-table.png"),
+    INSPECTION("inspection.png"),
+    INSTALLING_UPDATES("installing-updates.png"),
+    INTERNET_HUB("internet-hub.png"),
+    IRIS_SCAN("iris-scan.png"),
+
+    // K
+    KEYBOARD("keyboard.png"),
+    KEYPAD("keypad.png"),
+
+    // L
+    LANGUAGE("language.png"),
+    LIFEBUOY("lifebuoy.png"),
+    LINE_CHART("line-chart.png"),
+    LIST("list.png"),
+    LOW_PRIORITY("low-priority.png"),
+
+    // M
+    MAINTENANCE("maintenance.png"),
+    MANAGEMENT("management.png"),
+    MAXIMIZE_WINDOW("maximize-window.png"),
+    MERGE_CELLS("merge-cells.png"),
+    MICROSOFT_ACCESS("microsoft-access.png"),
+    MICROSOFT_EXCEL("microsoft-excel.png"),
+    MICROSOFT_WORD("microsoft-word.png"),
+    MINIMIZE_WINDOW("minimize-window.png"),
+    MINUS("minus.png"),
+    MODULE("module.png"),
+    MONDAY("monday.png"),
+    MONITOR("monitor.png"),
+    MOVE_NODE_DOWN("move-node-down.png"),
+    MOVE_NODE_UP("move-node-up.png"),
+
+    // N
+    NAME("name.png"),
+    NAME_TAG("name-tag.png"),
+    NAS("nas.png"),
+    NATURAL_GAS("natural-gas.png"),
+    NAVIGATION_TOOLBAR_BOTTOM("navigation-toolbar-bottom.png"),
+    NAVIGATION_TOOLBAR_LEFT("navigation-toolbar-left.png"),
+    NAVIGATION_TOOLBAR_TOP("navigation-toolbar-top.png"),
+    NETWORKING_MANAGER("networking-manager.png"),
+    NO_ACCESS("no-access.png"),
+    NO_NETWORK("no-network.png"),
+    NOTHING_FOUND("nothing-found.png"),
+    NOTIFICATION("notification.png"),
+    NUMBERED_LIST("numbered-list.png"),
+
+    // O
+    OBJECT("object.png"),
+    OK("ok.png"),
+    OPEN_BOOK("open-book.png"),
+    OPEN_PANE("open-pane.png"),
+
+    // P
+    PALM_SCAN("palm-scan.png"),
+    PASSWORD("password.png"),
+    PERSONAL_COMPUTER("personal-computer.png"),
+    PIN("pin.png"),
+    POPUP("popup.png"),
+    PRESENT("present.png"),
+    PRINT("print.png"),
+    PRINTER_MAINTENANCE("printer-maintenance.png"),
+    PRINTER_OUT_OF_PAPER("printer-out-of-paper.png"),
+    PRIVACY("privacy.png"),
+    PRODUCT("product.png"),
+
+    // Q
+    QUESTION_MARK("question-mark.png"),
+
+    // R
+    REPLACE("replace.png"),
+    REPLICATE_ROWS("replicate-rows.png"),
+    RESTORE_WINDOW("restore-window.png"),
+    RIGHT_NAVIGATION_TOOLBAR("right-navigation-toolbar.png"),
+    ROLLBACK("rollback.png"),
+
+    // S
+    SATURDAY("saturday.png"),
+    SAVE("save.png"),
+    SAVE_ALL("save-all.png"),
+    SAVE_AS("save-as.png"),
+    SAVE_CLOSE("save-close.png"),
+    SCAN_STOCK("scan-stock.png"),
+    SCHEDULE("schedule.png"),
+    SCHOOL("school.png"),
+    SCHOOL_DIRECTOR("school-director.png"),
+    SD("sd.png"),
+    SEARCH_MORE("search-more.png"),
+    SELECT_CELL("select-cell.png"),
+    SELECT_COLUMN("select-column.png"),
+    SELECT_ROW("select-row.png"),
+    SERVER("server.png"),
+    SERVER_SHUTDOWN("server-shutdown.png"),
+    SHUTDOWN("shutdown.png"),
+    SMS("sms.png"),
+    SPLIT_CELLS("split-cells.png"),
+    SPLIT_TABLE("split-table.png"),
+    SPREADSHEET("spreadsheet.png"),
+    STAFF("staff.png"),
+    STAFF_MENU("start-menu.png"),
+    STATISTICS("statistics.png"),
+    SUNDAY("sunday.png"),
+
+    // T
+    TABLE("table.png"),
+    TABLE_PROPERTIES("table-properties.png"),
+    TAG_WINDOW("tag-window.png"),
+    TASKS("tasks.png"),
+    TERMS_AND_CONDITIONS("terms-and-conditions.png"),
+    THURSDAY("thursday.png"),
+    TIME_MACHINE("time-machine.png"),
+    TIME_SPAN("time-span.png"),
+    TIMETABLE("timetable.png"),
+    TOUCH_ID("touch-id.png"),
+    TRASH("trash.png"),
+    TROLLEYBUS("trolleybus.png"),
+    TUESDAY("tuesday.png"),
+    TUNE("tune.png"),
+    TXT("txt.png"),
+
+    // U
+    UNDO("undo.png"),
+    UNINSTALL_PROGRAMS("uninstall-programs.png"),
+    UNINSTALLING_UPDATES("uninstalling-updates.png"),
+    UNPIN_2("unpin-2.png"),
+    UNPIN("unpin.png"),
+    UPDATE("update.png"),
+    UPLOAD("upload.png"),
+    UPLOAD_TO_FTP("upload-to-ftp.png"),
+    USB_2("usb-2.png"),
+    USB_CONNECTED("usb-connected.png"),
+    USB_DISCONNECTED("usb-disconnected.png"),
+    USB_OFF("usb-off.png"),
+    USB_ON("usb-on.png"),
+    USER_GROUP("user-group.png"),
+    USER_MALE("user-male.png"),
+    USER_MANUAL("user-manual.png"),
+
+    // V
+    VIEW("view.png"),
+    VOICE("voice.png"),
+
+    // W
+    WALL_MOUNT_CAMERA("wall-mount-camera.png"),
+    WEBCAM("webcam.png"),
+    WEDNESDAY("wednesday.png"),
+    WIFI_ROUTER("wifi-router.png"),
+    WISH_LIST("wish-list.png"),
+
+    // X
+    XLS("xls.png"),
+
+    // Y
+
+    // Z
+    ZOOM_IN("zoom-in.png"),
+    ZOOM_OUT("zoom-out.png"),
+    ZOOM_TO_ACTUAL_SIZE("zoom-to-actual-size.png"),
+    ZOOM_TO_EXTENTS("zoom-to-extents.png");
+
+    private String resourceName;
+
+    ImageResource(String resourceName) {
+        this.resourceName = resourceName;
     }
 
     public URL url() {
-        return ImageResource.class.getResource(this.resource + ".png");
+        return ImageResource.class.getResource(this.resourceName);
     }
 
     public static URL find(String resource) {
         return ImageResource.class.getResource(resource);
+    }
+
+    public String getResourceName() {
+        return resourceName;
     }
 }
